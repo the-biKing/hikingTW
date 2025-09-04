5af6e30fc0b7aabcbdea88eca775e239e04ddbff
the-biKing <147817629+the-biKing@users.noreply.github.com>
2025-09-04 22:33:12 +0800

    Update TimeCalculator.swift

--- HikingTimeAPP/Utils/TimeCalculator.swift
+++ HikingTimeAPP/Utils/TimeCalculator.swift
@@ -1,1 +1,13 @@
+import Foundation
+
+struct TimeCalculator {
+    static func estimatedTime(shangheMinutes: Double, factor: Double) -> Double {
+        return shangheMinutes * factor
+    }
+
+    static func standardShangheTime(actualMinutes: Double, factor: Double) -> Double {
+        guard factor != 0 else { return actualMinutes }
+        return actualMinutes / factor
+    }
+}
 
