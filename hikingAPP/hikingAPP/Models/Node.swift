61b43bf2c433ab754ad12aa8b0339fd90d086ca4
the-biKing <joshhsieh1129@gmail.com>
2025-09-15 12:16:21 +0800

    modified:   .DS_Store 	modified:   hikingAPP/hikingAPP/Models/Node.swift

--- hikingAPP/hikingAPP/Models/Node.swift
+++ hikingAPP/hikingAPP/Models/Node.swift
@@ -2,15 +2,12 @@ import Foundation
 import CoreLocation
 
 struct Node: Identifiable, Codable {
-    let id: UUID
+    let id: string
     let name: String
     let latitude: Double
     let longitude: Double
     let elevation: Double?
     let isCustom: Bool
 
-    var coordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-    }
 }
 
